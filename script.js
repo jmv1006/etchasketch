9c3b31d234142901baf70ec9e9e5f893fb7b974c
jonathanandradevalenzuela <jmv1006@gmail.com>
2021-10-18 11:18:41 -0700

    styles.css

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const container = document.getElementById("container");
 const grid = document.getElementById('grid');
-
-
 let gridSize;
+const btn = document.querySelector('button');
+
 
 function createGrid(gridSize) {
   grid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
@@ -10,10 +10,17 @@ function createGrid(gridSize) {
 
   for (i = 0; i < gridSize ** 2; i++) {
     const newDivs = document.createElement('div');
+    newDivs.addEventListener('mouseover', sketch)
     grid.appendChild(newDivs).className = 'boxes';
   }
 }
 createGrid(16);
+
+
+function sketch(e) {
+  e.target.style.backgroundColor = 'black';
+}
+
 
 
 
