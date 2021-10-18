3bd859a40c2665c5b554e14e19975852d3895c85
jonathanandradevalenzuela <jmv1006@gmail.com>
2021-10-18 10:17:05 -0700

    index.html

--- script.js
+++ script.js
@@ -1,30 +1,19 @@
 const container = document.getElementById("container");
+const grid = document.getElementById('grid');
 
 
+let gridSize;
 
-/* function createDiv() {
-  const newDiv = document.createElement('div');
-  container.appendChild(newDiv).className = 'grids ';
-  document.getElementById('container').appendChild(newDiv);
-}
-*/
+function createGrid(gridSize) {
+  grid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+  grid.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
 
-function createGrid() {
-  for (i = 0; i < 16; i++) {
-    const columns = document.createElement('div');
-    container.appendChild(columns).className = 'columns';
-
-    for (j = 0; j < 16; j++) {
-      const rows = document.createElement('div');
-      container.appendChild(rows).className = 'rows';
-    }
+  for (i = 0; i < gridSize ** 2; i++) {
+    const newDivs = document.createElement('div');
+    grid.appendChild(newDivs).className = 'boxes';
   }
 }
-
-createGrid();
-
-
-
+createGrid(16);
 
 
 
